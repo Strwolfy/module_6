07e1a361156331181c0280fbe25627156e5132fe
Strwolfy <strwolfy@gmail.com>
2024-12-02 17:00:45 +0300

    pull module_6_2.py

---
+++ module_6_2.py
@@ -0,0 +1,59 @@
+class Vehicle:
+    # Атрибут класса с допустимыми цветами
+    __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
+
+    def __init__(self, owner, model, color, engine_power):
+        self.owner = owner
+        self.__model = model
+        self.__color = color
+        self.__engine_power = engine_power
+
+    # Метод для получения модели
+    def get_model(self):
+        return f"Модель: {self.__model}"
+
+    # Метод для получения мощности двигателя
+    def get_horsepower(self):
+        return f"Мощность двигателя: {self.__engine_power}"
+
+    # Метод для получения цвета
+    def get_color(self):
+        return f"Цвет: {self.__color}"
+
+    # Метод для изменения цвета
+    def set_color(self, new_color):
+        if new_color.lower() in [color.lower() for color in self.__COLOR_VARIANTS]:
+            self.__color = new_color
+        else:
+            print(f"Нельзя сменить цвет на {new_color}")
+
+    # Метод для вывода информации об объекте
+    def print_info(self):
+        print(self.get_model())
+        print(self.get_horsepower())
+        print(self.get_color())
+        print(f"Владелец: {self.owner}")
+
+
+# Класс Sedan, наследующий Vehicle
+class Sedan(Vehicle):
+    __PASSENGERS_LIMIT = 5
+
+    def __init__(self, owner, model, color, engine_power):
+        super().__init__(owner, model, color, engine_power)
+
+
+# Проверка работы программы
+# Текущие цвета __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
+vehicle1 = Sedan('Fedos', 'Toyota Mark II', 'blue', 500)
+
+# Изначальные свойства
+vehicle1.print_info()
+
+# Меняем свойства (в т.ч. вызывая методы)
+vehicle1.set_color('Pink')
+vehicle1.set_color('BLACK')
+vehicle1.owner = 'Vasyok'
+
+# Проверяем что поменялось
+vehicle1.print_info()
